a32185238062fbbaa81e8c7b66033bd5f3595cf1
Samuel Van Pelt <samuelvanpelt@gmail.com>
2013-10-24 12:28:48 -0600

    test parser

--- test/sam_test_suite.rb
+++ test/sam_test_suite.rb
@@ -1,1 +1,2 @@
 require_relative "test_sequence.rb"
+require_relative "test_parser.rb"
---
+++ test/test_parser.rb
@@ -0,0 +1,9 @@
+#require_relative "../src/parser.rb"
+
+require "test/unit"
+
+class TestParser < Test::Unit::TestCase
+	def test_autopass
+		assert(true)
+	end	
+end 
