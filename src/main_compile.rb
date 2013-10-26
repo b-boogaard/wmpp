7b2d032e9996c29cbf35a8b20c16b31a450060d9
Samuel Van Pelt <samuelvanpelt@gmail.com>
2013-10-25 22:32:43 -0600

    made main_compile more readable

--- src/main_compile.rb
+++ src/main_compile.rb
@@ -3,14 +3,13 @@ require_relative "stringsequence.rb"
 require_relative "jasm_translate.rb"
 
 def compile(file)
-	s = File.read(file)
-	t = Tokens.new(StringSequence.new(s))
-	g = RecursiveDescentParser.new
-	e = g.parse(t)
-    r = JasminTranslator.new
-    j = file[0..-6]
-    j += ".j"
-    r.translate(j,e)
+	sequence = File.read(file)
+	tokens = Tokens.new(StringSequence.new(sequence))
+	parser = RecursiveDescentParser.new
+	statements = parser.parse(tokens)
+    translator = JasminTranslator.new
+    filename = file[0..-6] + ".j"
+    translator.translate(filename,statements)
 end
 
 ARGV.each do |a|
