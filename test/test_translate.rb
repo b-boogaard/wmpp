fbc00101f730e45c7c052ff81e51956c7df3e45b
Samuel Van Pelt <samuelvanpelt@gmail.com>
2013-10-26 00:59:19 -0600

    translate tests

--- test/test_translate.rb
+++ test/test_translate.rb
@@ -4,12 +4,20 @@ require_relative "../src/jasm_translate.rb"
 
 
 require "test/unit"
+
+class Float
+	 def sigfig_to_s(digits)
+		f = sprintf("%.#{digits - 1}e", self).to_f
+		i = f.to_i
+		(i == f ? i : f).to_s
+	  end
+end
+
 class TestParser < Test::Unit::TestCase
 	def test_translate_all
 		Dir.foreach("#{Dir.pwd}/test/wmpp") do |item|
-			next if item == '.' or item == '..'
+			next if item == '.' or item == '..' or item == '.DS_Store'
 			path = "#{Dir.pwd}/test/wmpp/#{item}"
-			s = File.read(path)
 			syspath = "ruby" + " #{Dir.pwd}/src/main_compile.rb test/wmpp/#{item}"
 			puts syspath
 			system(syspath)
@@ -22,6 +30,32 @@ class TestParser < Test::Unit::TestCase
 			sys = "java -jar jasmin.jar -d " + "#{dir}/bin " + "#{dir}/j/#{j}"
 			puts sys
 			system(sys)
+
+			sys_jav = "java -classpath " + "#{dir}/bin #{j[0..-3]}" + " > #{dir}/tmp/results.txt"
+			system(sys_jav)
+			j = File.read("#{dir}/tmp/results.txt")
+
+			array = Array.new
+
+			j.each_line do |line|
+				array << line
+			end
+ 
+			array.reverse!
+			s = File.read(path)
+			g = RecursiveDescentParser.new
+
+			t = Tokens.new(StringSequence.new(s))
+			e = g.parse(t)
+			e.statements.reverse!
+
+			i = 0
+			e.statements.each do |statement|
+				javaline = array[i]
+				assert_equal(statement.eval.to_f.sigfig_to_s(4), javaline.to_f.sigfig_to_s(4), "#{statement.eval} != #{javaline}")
+				i += 1
+			end
+			
 		end 
 	end
 end
