da70188cdfe2898a77b9568152d8995f035a2e37
Samuel Van Pelt <samuelvanpelt@gmail.com>
2013-11-04 12:32:14 -0700

    less than working

--- src/ast_compare.rb
+++ src/ast_compare.rb
@@ -43,10 +43,10 @@ def eval
       out.write("fcmpl\n")
       out.write("ifeq Tru#{$modf}\n")
       out.write("goto Fal#{$modf}\n")
-      out.write("Tru#{$modf}:\n")
+      out.write("Fal#{$modf}:\n")
       out.write("ldc 1.0\n")  
       out.write("goto Endy#{$modf}\n")  
-      out.write("Fal#{$modf}:\n")
+      out.write("Tru#{$modf}:\n")
       out.write("ldc 0.0\n")    
       out.write("Endy#{$modf}:\n") 
       $modf+=1
