30a83a178240a937e1d43c2dca0978bb94435d20
Brian van de Boogaard <brian.boogaard@gmail.com>
2013-11-07 12:33:31 -0700

    fixed brian_test_suite

--- test/brian_test_suite.rb
+++ test/brian_test_suite.rb
@@ -1,7 +1,6 @@
 require_relative "test_token.rb"
 require_relative "test_tokens.rb"
 require_relative "test_ast_node.rb"
-require_relative "test_ast_symbol.rb"
 require_relative "test_ast_number.rb"
 require_relative "test_ast_sum.rb"
 require_relative "test_ast_product.rb"
