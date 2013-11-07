a5ebe1485712a818f4829dff23960a1e6af64acf
Brian van de Boogaard <brian.boogaard@gmail.com>
2013-11-07 12:31:46 -0700

    removed test_ast_symbol.rb

--- test/test_ast_symbol.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../src/ast_symbol.rb"
-require "test/unit"
-
-class TestASTSymbol < Test::Unit::TestCase
-	def test_eval
-		symbol = ASTSymbol.new("x")
-		assert_equal("x", symbol.eval)
-	end
-end
